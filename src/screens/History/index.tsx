79fe243205b034038241d6208797ae13171af633
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-15 23:12:47 -0300

    :sparkles: screen history

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { SectionList, Heading, Text, VStack } from "native-base";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "21.12.23",
+      data: ["puxada", "levantamento"]
+    },
+    {
+      title: "10.12.23",
+      data: ["puxada", "levantamento"]
+    }
+  ])
+
   return (
-    <Center flex={1}>
-      <Text color={"white"}>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Ainda não realizou nenhuma atividade. {"\n"}
+            Vamos treinar ?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   );
 }
