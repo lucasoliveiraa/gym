d8b405df2c029fc1ac5ed71b3e4c3eeaa9e798bd
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-15 23:12:57 -0300

    :sparkles: screen profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,55 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { TouchableOpacity } from "react-native";
+import { Input } from "@components/Input";
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [loadingPhoto, setLoadingPhoto] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text color={"white"}>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {
+          loadingPhoto ?
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          :
+            <UserPhoto
+              source={{ uri: 'https://github.com/lucasoliveiraa.png'}}
+              size={33}
+              alt="Image user"
+            />
+          }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="gray.600"
+            value="lucas.oliveira@gmail.com"
+            isDisabled
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
