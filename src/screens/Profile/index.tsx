7c8d77ef20b332beab052cf534e1dff1933d86c8
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-22 21:30:46 -0300

    ✨ feat: create screen profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 const PHOTO_SIZE = 33
 
@@ -13,7 +14,7 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {
           loadingPhoto ?
@@ -49,6 +50,29 @@ export function Profile() {
             isDisabled
           />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+          <Input
+            bg="gray.600"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+          <Button title="Atualizar" />
+        </VStack>
       </ScrollView>
     </VStack>
   );
