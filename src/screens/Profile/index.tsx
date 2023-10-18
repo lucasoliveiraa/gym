de416889bd68607468075ebc6dd4923f4bfb75d0
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-10-17 22:40:29 -0300

    ✨ feat: react-hook-form and yup

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,15 +1,59 @@
-import { useState } from "react";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
-import { ScreenHeader } from "@components/ScreenHeader";
-import { UserPhoto } from "@components/UserPhoto";
-import { TouchableOpacity } from "react-native";
-import { Input } from "@components/Input";
-import { Button } from "@components/Button";
+import { useState } from 'react';
+import { Alert, TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
-const PHOTO_SIZE = 33
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
-  const [loadingPhoto, setLoadingPhoto] = useState(false)
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/lucasoliveiraa.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelected(){
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if(photoSelected.cancelled) {
+        return;
+      }
+
+      if(photoSelected.uri) {
+
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
+
+        if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
+
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.uri);
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -17,7 +61,7 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {
-          loadingPhoto ?
+          photoIsLoading  ?
             <Skeleton
               w={PHOTO_SIZE}
               h={PHOTO_SIZE}
@@ -27,13 +71,15 @@ export function Profile() {
             />
           :
             <UserPhoto
-              source={{ uri: 'https://github.com/lucasoliveiraa.png'}}
-              size={33}
+              source={{ uri: userPhoto }}
+              size={PHOTO_SIZE}
               alt="Image user"
             />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={handleUserPhotoSelected}
+          >
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>
@@ -52,7 +98,7 @@ export function Profile() {
         </Center>
 
         <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading color="gray.200" fontSize="md" mb={2} fontFamily="heading">
             Alterar senha
           </Heading>
 
